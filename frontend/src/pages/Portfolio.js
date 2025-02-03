6239bbdb9c82adabf085fb9bbddb3c08021c1e63
Calvin Tai <calvintai7777@gmail.com>
2025-02-02 23:22:34 -0800

    Basic Components working

---
+++ frontend/src/pages/Portfolio.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Portfolio = () => {
+    return (
+        <div>
+            <h1>Please check out my work/projects!</h1>
+        </div>
+    );
+};
+
+export default Portfolio;
